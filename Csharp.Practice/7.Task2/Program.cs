f384c111fcab9db8463371c92b68dbb332539cad
Pavel Petrusevich <p.petrusevich@reply.com>
2022-03-18 21:58:36 +0300

    add practice

---
+++ Csharp.Practice/7.Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿object[] objects = new object[3];
+objects[0] = 32;
+objects[1] = 'A';
+objects[2] = "Hello";
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine(objects[i]);
+}
+
+objects[0] = (int)objects[0]+10;
+objects[2] = objects[2]+", guys!";
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine(objects[i]);
+}
+
+Console.ReadKey();
---
+++ Csharp.Practice/7.Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] numbers = new int[13];
+Random random = new Random();
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = random.Next(1,100);
+}
+for 
+{
+
+}
