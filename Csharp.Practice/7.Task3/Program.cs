ca9d1390f9c20d72f9c522047e9ef2f9ed8ca4e7
Pavel Petrusevich <p.petrusevich@reply.com>
2022-03-18 22:02:01 +0300

    Add 3

--- Csharp.Practice/7.Task3/Program.cs
+++ Csharp.Practice/7.Task3/Program.cs
@@ -4,7 +4,9 @@ for (int i = 0; i < numbers.Length; i++)
 {
     numbers[i] = random.Next(1,100);
 }
-for 
+for (int i = 0; i < numbers.Length; i++)
 {
-
+    Console.WriteLine(numbers[i]);
 }
+Console.WriteLine(numbers.Max());
+Console.ReadKey();
