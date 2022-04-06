cbf299190840c9f573c8cd730671f64f446f7cc2
Pavel Petrusevich <p.petrusevich@reply.com>
2022-04-06 20:59:51 +0300

    add task1

---
+++ Lecture_13/Lecture_13/Motorcycle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Motorycle_Configuration
+{
+    internal class Motorcycle
+    {
+        public string Code { get; set; }
+
+        public string Model { get; set; }
+
+        public string Manufacturer { get; set; }
+
+        public DateOnly DateOnly { get; set; }
+
+        public int Run { get; set; }
+
+        public void RetrieveInformation()
+        {
+            Console.WriteLine("\nInformatoin:");
+            Console.WriteLine($"Code: {Code}.");
+            Console.WriteLine($"Model: {Model}.");
+            Console.WriteLine($"Manufacturer: {Manufacturer}.");
+            Console.WriteLine($"Year of issue: {DateOnly}.");
+            Console.WriteLine($"Mileage: {Run}.");
+        }
+    }
+}
---
+++ Lecture_13/Lecture_13/Program.cs
@@ -0,0 +1,17 @@
+﻿using Motorycle_Configuration;
+
+Motorcycle moto = new Motorcycle();
+{
+    string Code = Guid.NewGuid().ToString();
+    string Model = "Honda";
+    string Manufacturer = "Japan";
+    DateOnly Year of issue = new DateOnly();
+    int Mileage = 100;
+};
+
+var moto = new[] {moto};
+
+foreach (var item in moto)
+{
+    item.RetrieveInformation();
+}
